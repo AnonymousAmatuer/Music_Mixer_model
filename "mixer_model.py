f2891ac88930513935e08e67fd0452ff14852d64
AnonymousAmatuer <86914725+AnonymousAmatuer@users.noreply.github.com>
2021-11-26 17:42:24 +0400

    Create "mixer_model.py

---
+++ "mixer_model.py
@@ -0,0 +1,17 @@
+#@title  The Mixer (The Music Mixer, model)
+Volume = 67 #@param {type: "slider", min: 0, max: 100}
+EQ = 13 #@param {type: "slider" ,min:0, max:100}
+Pan = 41 #@param {type: "slider",min:0, max:100}
+Reverb = 83 #@param {type:"slider", min:0, max:100, step:1}
+Compression = 56  #@param {type: "slider", min: 0, max: 100}
+Aux = 89  #@param {type: "slider", min: 0, max: 100}
+Master_Gain = 54  #@param {type: "slider", min: 0, max: 100}
+Click-the-correct-function  = "Solo" #@param ["Mute", "Solo"]
+#@markdown  -----------------------------------------MAIN MIX--------------------------------------------
+L = 34 #@param {type:"slider", min:0, max:50, step:0}
+R = 28 #@param {type:"slider", min:0, max:50, step:0}
+#@markdown This is the model of a Mixer. If there are any bugs, please feel free to connect and correct my mistakes on this model.
+
+
+#End of project
+#Use google colab for this project
